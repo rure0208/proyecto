3e4963572bf7e68010b849aebe458a0733351ab7
Efrain_R_Ruiz <79539402+rure0208@users.noreply.github.com>
2022-10-12 16:14:24 -0700

    loginvista

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,40 @@
 import React from 'react'
 import {Carousel} from '@mantine/carousel';
-import { BackgroundImage,Image, Center, Text, Box } from '@mantine/core';
+import {  TextInput, Checkbox, Button, Group, Box,PasswordInput  } from '@mantine/core';
 import Layout from './compionents/Layout';
+import { useForm } from '@mantine/form';
 
 const Home = () => {
   return (
     <div>
 
     <Layout tituloPagina="Inicio">
-    <Carousel slideSize="70%" height={200} slideGap="md" loop withControls={false} withIndicators>
-    <Carousel.Slide>
-      <Image
-        src=" https://st4.depositphotos.com/20547288/23159/i/600/depositphotos_231598016-stock-photo-abstract-purple-smoke-mist-fog.jpg"
-        radius="sm"/>
-      </Carousel.Slide>
-      <Carousel.Slide> 
-        <Image
-        src="https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
-        radius="sm"/>
-        </Carousel.Slide>
-  
-        <Carousel.Slide>
-      <Image
-        src=" https://st4.depositphotos.com/20547288/23159/i/600/depositphotos_231598016-stock-photo-abstract-purple-smoke-mist-fog.jpg"
-        radius="sm"/>
-      </Carousel.Slide>
-      <Carousel.Slide> 
-        <Image
-        src="https://images.unsplash.com/photo-1419242902214-272b3f66ee7a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80"
-        radius="sm"/>
-        </Carousel.Slide>    
-      {/* ...other slides */}
-    </Carousel>
+    <Box sx={{ maxWidth: 300 }} mx="auto">
+      
+        <TextInput
+          withAsterisk
+          label="Email"
+          placeholder="your@email.com"
+          
+        />
+        <PasswordInput 
+          withAsterisk
+          label="Contraseña"
+          
+          
+        />
+
+        <Checkbox
+          mt="md"
+          label="Recordar Credenciales"
+          
+        />
+
+        <Group position="right" mt="md">
+          <Button type="submit">Submit</Button>
+        </Group>
+      
+    </Box>
 
       
     </Layout>
